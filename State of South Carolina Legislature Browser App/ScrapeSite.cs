0962a320464d07232e88bd46684d2bcc94367b8c
Jesse Ayala <jesserayala95@gmail.com>
2022-02-26 15:04:29 -0500

    Created class in charge of scraping the data it needs. Added NuGet HTMLAgilityPack for easier parsing of the HTML

---
+++ State of South Carolina Legislature Browser App/ScrapeSite.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State_of_South_Carolina_Legislature_Browser_App
+{
+	class ScrapeSite
+	{
+
+	}
+}
