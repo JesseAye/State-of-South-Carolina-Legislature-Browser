89a490cfbd9abcdd268c017f9ccc523cc1f2b3a8
Jesse Ayala <jesserayala95@gmail.com>
2022-02-27 18:29:37 -0500

    Began adding documentation to DataModels.cs Started coding logic for digesting the scraped data

--- State of South Carolina Legislature Browser App/DataModels.cs
+++ State of South Carolina Legislature Browser App/DataModels.cs
@@ -7,20 +7,37 @@ using System.Threading.Tasks;
 namespace State_of_South_Carolina_Legislature_Browser_App
 {
 	/// <summary>
+	/// Singleton that holds the structure of the Code of Laws<para/>
 	/// <see href="https://csharpindepth.com/articles/singleton">Creating singleton class structure</see>, uses Fourth version
 	/// </summary>
 	public class CodeOfLaws
 	{
-		private const string URL = "https://www.scstatehouse.gov/code/statmast.php";
+		public const string SubDirectory = "/code/statmast.php";
 
-		public List<Title> Titles { get; } = new List<Title>();
+		/// <summary>
+		/// The sub-unit of the Code of Laws
+		/// </summary>
+		public static List<Title> Titles { get; } = new List<Title>();
 
+		/// <summary>
+		/// Create a single static instance of this class to become a singleton
+		/// </summary>
 		private static readonly CodeOfLaws _instance = new CodeOfLaws();
 
+		/// <summary>
+		/// Not entirely too sure why this is required, but I believe it has something to do with how this class is compiled and telling compiler/interpreter that this will use singleton pattern<para/>
+		/// <see href="https://csharpindepth.com/articles/BeforeFieldInit">See Article</see>
+		/// </summary>
 		static CodeOfLaws() { }
 
+		/// <summary>
+		/// Private constructor as part of requirement of the <c>beforefieldinit</c> thing for singleton pattern 
+		/// </summary>
 		private CodeOfLaws() { }
 
+		/// <summary>
+		/// Provide's the single instance of this class
+		/// </summary>
 		public static CodeOfLaws Instance
 		{
 			get
@@ -29,22 +46,49 @@ namespace State_of_South_Carolina_Legislature_Browser_App
 			}
 		}
 
-		public static bool AddTitle(string url)
+		public bool AddTitle(Title title)
 		{
-			return true;
+			if (!Titles.Contains(title))
+			{
+				Titles.Add(title);
+				return true;
+			}
+
+			else
+			{
+				//TODO: After XML Implementation - Add check to see if any Titles shifted/changed name
+				return false;
+			}
 		}
 	}
 
-	public class Title
+	public abstract class LawSubUnit
 	{
+		/// <summary>
+		/// The URL of this sub-unit
+		/// </summary>
 		public string URL { get; set; }
 
+		/// <summary>
+		/// The description/identifier of the sub-unit
+		/// </summary>
 		public string Description { get; set; }
-
-		public List<Chapter> Chapters { get; } = new List<Chapter>();
-
 	}
 
+	/// <summary>
+	/// Holds the structure of the Titles in the law
+	/// </summary>
+	public class Title : LawSubUnit
+	{
+		/// <summary>
+		/// The <see cref="List{Chapter}">List of Chapters</see> within the parent object 
+		/// </summary>
+		public List<Chapter> Chapters { get; } = new List<Chapter>();
+	}
+
+	/// <summary>
+	/// Holds the structure of the <see cref="Chapter">Chapters</see> within <see cref="Title">Titles</see>
+	/// </summary>
 	public class Chapter
 	{
 		public string URL { get; set; }
@@ -54,6 +98,9 @@ namespace State_of_South_Carolina_Legislature_Browser_App
 		public List<Article> Articles { get; } = new List<Article>();
 	}
 
+	/// <summary>
+	/// Holds the structure of the <see cref="Article">Articles</see> within <see cref="Chapter">Chapters</see>
+	/// </summary>
 	public class Article
 	{
 		public string URL { get; set; }
@@ -63,6 +110,9 @@ namespace State_of_South_Carolina_Legislature_Browser_App
 		public List<Section> Sections { get; } = new List<Section>();
 	}
 
+	/// <summary>
+	/// Holds the structure of the <see cref="Section">Sections</see> within <see cref="Article">Articles</see>
+	/// </summary>
 	public class Section
 	{
 		public string URL { get; set; }
--- State of South Carolina Legislature Browser App/MainForm.cs
+++ State of South Carolina Legislature Browser App/MainForm.cs
@@ -1,4 +1,6 @@
 ﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -7,7 +9,7 @@ namespace State_of_South_Carolina_Legislature_Browser_App
 	public partial class MainForm : Form
 	{
 		private CodeOfLaws CodeOfLaws = CodeOfLaws.Instance;
-		private const string HomeURI = "https://www.scstatehouse.gov";
+		private const string Domain = "https://www.scstatehouse.gov";
 
 		public MainForm()
 		{
@@ -17,12 +19,23 @@ namespace State_of_South_Carolina_Legislature_Browser_App
 
 		private void LoadPage()
 		{
-			HtmlAgilityPack.HtmlDocument COLHome = ScrapeSite.GetPage($"https://www.scstatehouse.gov/code/statmast.php");
+			HtmlAgilityPack.HtmlDocument COLHome = ScrapeSite.GetPage(Domain + CodeOfLaws.SubDirectory);
 
-			var TitleNodes = COLHome.DocumentNode.SelectNodes("//div[@id=\"contentsection\"]/a")
+			string XPath = "//div[@id=\"contentsection\"]/a";
+
+			List<Title> TitleNodes = COLHome.DocumentNode.SelectNodes(XPath)
 													.Where(a => a.InnerText.StartsWith("Title"))
-													.Select(title => new Title() { URL = HomeURI + title.Attributes["href"].Value, Description = title.NextSibling.InnerText.Substring(3) })
+													.Select(title => new Title() { URL = Domain + title.Attributes["href"].Value, Description = title.NextSibling.InnerText.Substring(3) })
 													.ToList();
+
+			foreach (Title title in TitleNodes)
+			{
+				if (!CodeOfLaws.AddTitle(title))
+				{
+					throw new Exception($"Unable to CodeOfLaws.AddTitle() with the following information:\n{title.URL}\n{title.Description}");
+				}
+				
+			}
 		}
 	}
 }
